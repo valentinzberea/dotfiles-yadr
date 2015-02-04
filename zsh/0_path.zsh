a97c4401047afb77350610753674ce3a43a5f499
Valentin Zberea <valentin.zberea@gmail.com>
2015-02-04 18:34:30 +0200

    Add workon bindings

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,2 +1,4 @@
 # path, the 0 in the filename causes this to load first
 export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr
+export WORKON_HOME=~/.virtualenv
+source /usr/local/bin/virtualenvwrapper.sh
