e45539c01204e8d9114d4d1f78f1b72661b2bb52
Valentin Zberea <valentin.zberea@gmail.com>
2015-09-27 17:22:26 +0300

    Remove virtualenvwrapper

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,7 +1,5 @@
 # path, the 0 in the filename causes this to load first
 export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr
-export WORKON_HOME=~/.virtualenv
-source /usr/local/bin/virtualenvwrapper.sh
 path=(
   $path
   $HOME/.yadr/bin
